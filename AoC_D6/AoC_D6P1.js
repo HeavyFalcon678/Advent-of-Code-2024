004fffdd6bc6d9dc463ed23b072930c1329c36d5
HeavyFalcon678 <heavyfalcon678@gmail.com>
2025-05-22 22:17:43 -0700

    D6P1: split into grid and get coordinates of obstacles and guard

--- AoC_D6/AoC_D6P1.js
+++ AoC_D6/AoC_D6P1.js
@@ -2,7 +2,7 @@
 const fs = require("node:fs");
 
 // test with example input 
-const debug = false;
+const debug = true;
 let input;
 
 // Get the input; if in debug mode use example input, else use the actual input
@@ -20,3 +20,25 @@ if (!debug) {
 #.........
 ......#...`;
 }
+
+
+
+input = input.split("\n");
+let grid = [];
+
+input.forEach(line => grid.push(line.split("")));
+
+let pos;
+let obstacles = [];
+let direction = "N";
+
+grid.forEach((row, rowIndex) => {
+   row.forEach((col, colIndex) => {
+    if (col == "#") obstacles.push([rowIndex, colIndex]);
+    if (col == "^") pos = [rowIndex, colIndex];
+   }) 
+});
+
+console.log(obstacles);
+
+console.log(pos);
